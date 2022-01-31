89fd71decfb55855d6f1bdb2b2097c89bf1b7d58
JoshuaO22 <94143587+JoshuaO22@users.noreply.github.com>
2022-01-31 17:18:57 -0500

    Update ColorDetection.java

--- MagicBox/src/main/java/frc/robot/subsystems/ColorDetection.java
+++ MagicBox/src/main/java/frc/robot/subsystems/ColorDetection.java
@@ -31,7 +31,7 @@ public class ColorDetection extends SubsystemBase {
     // Creates new color sensor and shufffleboard
     m_colorSensor = new ColorSensorV3(I2C.Port.kOnboard);
     m_controlPanelTab = Shuffleboard.getTab(Constants.kShuffleboardTab);
-    m_controlPanelStatus = m_controlPanelTab.getLayout("Status", BuiltInLayouts.kList)
+    m_controlPanelStatus = m_controlPanelTab.getLayout("Color Status", BuiltInLayouts.kList)
       .withSize(3, 4)
       .withProperties(Map.of("Label position", "TOP"));
 
