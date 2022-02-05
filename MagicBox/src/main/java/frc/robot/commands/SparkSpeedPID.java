33d8924fb95cbed0d60e7f76f0915a9b9e05dacd
JoshuaO22 <94143587+JoshuaO22@users.noreply.github.com>
2022-02-05 09:31:42 -0500

    add drivetrain to SparkSpeedPID

--- MagicBox/src/main/java/frc/robot/commands/SparkSpeedPID.java
+++ MagicBox/src/main/java/frc/robot/commands/SparkSpeedPID.java
@@ -4,30 +4,30 @@
 
 package frc.robot.commands;
 
-import com.revrobotics.CANSparkMax;
-
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
+import frc.robot.subsystems.DrivetrainSparks;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class SparkSpeedPID extends PIDCommand {
   /** Creates a new SparkSpeedPID. */
-  public SparkSpeedPID(CANSparkMax motor, double speed) {
+  public SparkSpeedPID(DrivetrainSparks drivetrain, double speed) {
     super(
         // The controller that the command will use
         new PIDController(0, 0, 0),
         // This should return the measurement
-        () -> motor.getEncoder().getVelocity(),
+        () -> drivetrain.leftSpark.getEncoder().getVelocity(),
         // This should return the setpoint (can also be a constant)
         () -> speed,
         // This uses the output
         output -> {
           // Use the output here
-          motor.set(output);
+          drivetrain.leftSpark.set(output);
         });
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(drivetrain);
     // Configure additional PID options by calling `getController` here.
   }
 
