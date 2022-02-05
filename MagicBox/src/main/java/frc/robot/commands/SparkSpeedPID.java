fcb5c8b934f09540487ef48dcc32fea9d63706d1
JoshuaO22 <94143587+JoshuaO22@users.noreply.github.com>
2022-02-05 10:35:45 -0500

    Update SparkSpeedPID.java
    set integral coefficient
    clamp output
    set tolerance of 10
    command ends at setpoint

--- MagicBox/src/main/java/frc/robot/commands/SparkSpeedPID.java
+++ MagicBox/src/main/java/frc/robot/commands/SparkSpeedPID.java
@@ -4,6 +4,7 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDCommand;
 import frc.robot.subsystems.DrivetrainSparks;
@@ -16,7 +17,7 @@ public class SparkSpeedPID extends PIDCommand {
   public SparkSpeedPID(DrivetrainSparks drivetrain, double speed) {
     super(
         // The controller that the command will use
-        new PIDController(0, 0, 0),
+        new PIDController(0, 0.001, 0),
         // This should return the measurement
         () -> drivetrain.getLeftSpeed(),
         // This should return the setpoint (can also be a constant)
@@ -24,16 +25,17 @@ public class SparkSpeedPID extends PIDCommand {
         // This uses the output
         output -> {
           // Use the output here
-          drivetrain.setLeftSpeed(output);;
+          drivetrain.setLeftSpeed(MathUtil.clamp(output, -1, 1));;
         });
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drivetrain);
     // Configure additional PID options by calling `getController` here.
+    getController().setTolerance(10);
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return getController().atSetpoint();
   }
 }
