f47fa8cabbd447174464905208893c7748bbef65
AkshayM1 <122124@lrstudents.org>
2023-02-06 17:27:31 -0500

    Branch for vision detection
    commands to switch pipelines, and x, y, and area is displayed on the smartdashboard

---
+++ MagicBox-Imported/src/main/java/frc/robot/subsystems/Vision.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Vision extends SubsystemBase {
+  /** Creates a new Vision. */
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry tx = table.getEntry("tx");
+  NetworkTableEntry ty = table.getEntry("ty");
+  NetworkTableEntry ta = table.getEntry("ta");
+  public Vision() {
+
+  }
+
+  public void setPipeline(double pipeline) {
+    table.getEntry("pipeline").setNumber(pipeline);
+  }
+
+  public void setLight(double value) {
+    table.getEntry("ledMode").setNumber(value);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    double x = tx.getDouble(0.0);
+    double y = ty.getDouble(0.0);
+    double area = ta.getDouble(0.0);
+
+    SmartDashboard.putNumber("LimelightX", x);
+    SmartDashboard.putNumber("LimelightY", y);
+    SmartDashboard.putNumber("LimelightArea", area);
+  }
+}
