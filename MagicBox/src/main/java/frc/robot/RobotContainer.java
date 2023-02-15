be0d2dac49d76e713509f74e6b36159cc911a00a
PraveenPrabaharan <83364745+PraveenPrabaharan@users.noreply.github.com>
2023-02-15 17:49:19 -0500

    Same as previous commit

--- MagicBox/src/main/java/frc/robot/RobotContainer.java
+++ MagicBox/src/main/java/frc/robot/RobotContainer.java
@@ -35,56 +35,56 @@ import static frc.robot.Constants.TelescopingConstants.*;
  */
 public class RobotContainer {
   // Controller
-  // private final XboxController m_driver = new XboxController(Constants.DrivetrainConstants.kDriverControllerPort);
+  private final XboxController m_driver = new XboxController(Constants.DrivetrainConstants.kDriverControllerPort);
   private final XboxController m_weapons = new XboxController(Constants.DrivetrainConstants.kWeaponsControllerPort);
 
-  // private SlewRateLimiter m_forwardLimiter = new SlewRateLimiter(1); // controls acceleration of forward speed
-  // private SlewRateLimiter m_rotationLimiter = new SlewRateLimiter(0.5); // controls acceleration of rotational speed
+  private SlewRateLimiter m_forwardLimiter = new SlewRateLimiter(1); // controls acceleration of forward speed
+  private SlewRateLimiter m_rotationLimiter = new SlewRateLimiter(0.5); // controls acceleration of rotational speed
 
   // Subsystems
   private final Lights m_lights;
   private final Gripper m_gripper;
-  // private final Drivetrain m_drivetrain;
-  // private final PivotArm m_PinkArm;
-  // private final TelescopingArm m_arm;
+  private final Drivetrain m_drivetrain;
+  private final PivotArm m_PinkArm;
+  private final TelescopingArm m_arm;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Subsystems Instantiation
     m_gripper = new Gripper();
     m_lights = new Lights();
-    // m_drivetrain = new Drivetrain();
-    // m_PinkArm = new PivotArm();
-    // m_arm = new TelescopingArm();
+    m_drivetrain = new Drivetrain();
+    m_PinkArm = new PivotArm();
+    m_arm = new TelescopingArm();
 
     // Setting default commands
-    // m_arm.setDefaultCommand(
-    //   new ArmControl(() -> m_weapons.getLeftY(), m_arm));
+    m_arm.setDefaultCommand(
+      new ArmControl(() -> m_weapons.getLeftY(), m_arm));
 
     // Control Panel
     new ControlPanel(m_gripper, m_lights);
 
     // Setting default commands
-    // m_arm.setDefaultCommand(
-    //   new ArmControl(() -> m_weapons.getLeftY(), m_arm));
+    m_arm.setDefaultCommand(
+      new ArmControl(() -> m_weapons.getLeftY(), m_arm));
 
     // Lights
     m_lights.setDefaultCommand(new CheckObjectColor(m_gripper, m_lights));
 
     // sets the drivetrain default command to curvatureDrive, with the slewratelimiters
     // Left Joystick: forwards/backward, Right Joystick: turn in place left/right
-    // m_drivetrain.setDefaultCommand(
-    // new curvatureDrive(
-    //   () -> Math.copySign(Constants.DrivetrainConstants.kS, m_driver.getLeftY())
-    //   + m_forwardLimiter.calculate(m_driver.getLeftY() / Drivetrain.speedLimiter), 
-    //   () -> Math.copySign(Constants.DrivetrainConstants.kS, m_driver.getRightX()) 
-    //   + m_rotationLimiter.calculate(m_driver.getRightX() / Drivetrain.rotationLimiter),
-    //   () -> true, m_drivetrain));
+    m_drivetrain.setDefaultCommand(
+    new curvatureDrive(
+      () -> Math.copySign(Constants.DrivetrainConstants.kS, m_driver.getLeftY())
+      + m_forwardLimiter.calculate(m_driver.getLeftY() / Drivetrain.speedLimiter), 
+      () -> Math.copySign(Constants.DrivetrainConstants.kS, m_driver.getRightX()) 
+      + m_rotationLimiter.calculate(m_driver.getRightX() / Drivetrain.rotationLimiter),
+      () -> true, m_drivetrain));
     
     // // Pink Arm
-    // m_PinkArm.setDefaultCommand(
-    //   new armJoint(() -> m_weapons.getRightY(), m_PinkArm)
-    // );
+    m_PinkArm.setDefaultCommand(
+      new armJoint(() -> m_weapons.getRightY(), m_PinkArm)
+    );
 
     // Configure the button bindings
 
@@ -104,15 +104,15 @@ public class RobotContainer {
     new JoystickButton(m_weapons, Button.kLeftStick.value).onTrue(new ChangeLEDColor(m_lights, 255, 255, 0));
     new JoystickButton(m_weapons, Button.kRightStick.value).onTrue(new ChangeLEDColor(m_lights, 101, 15, 140));
 
-    // new JoystickButton(m_weapons, Button.kY.value).toggleOnTrue(new ExtendVal( TelescopingConstants.HighExtendCube, m_arm));
-    // new JoystickButton(m_weapons, Button.kX.value).toggleOnTrue(new ExtendVal( TelescopingConstants.MidExtendCube, m_arm));
-    // new JoystickButton(m_weapons, Button.kA.value).toggleOnTrue(new ExtendVal( TelescopingConstants.LowStop , m_arm));
+    // new JoystickButton(m_weapons, Button.kY.value).onTrue(new ExtendVal( TelescopingConstants.HighExtendCube, m_arm));
+    // new JoystickButton(m_weapons, Button.kX.value).onTrue(new ExtendVal( TelescopingConstants.MidExtendCube, m_arm));
+    // new JoystickButton(m_weapons, Button.kA.value).onTrue(new ExtendVal( TelescopingConstants.LowStop , m_arm));
 
-    // new JoystickButton(m_weapons, Button.kLeftBumper.value).onTrue(new AutoScore(m_PinkArm, m_arm, m_gripper, kHighAngleCone, HighExtendCone));
-    // new JoystickButton(m_weapons, (int) m_weapons.getLeftTriggerAxis()).onTrue(new AutoScore(m_PinkArm, m_arm, m_gripper, kMidAngleCone, MidExtendCone));
+    new JoystickButton(m_weapons, Button.kLeftBumper.value).onTrue(new AutoScore(m_PinkArm, m_arm, m_gripper, kHighAngleCone, HighExtendCone));
+    new JoystickButton(m_weapons, (int) m_weapons.getLeftTriggerAxis()).onTrue(new AutoScore(m_PinkArm, m_arm, m_gripper, kMidAngleCone, MidExtendCone));
 
-    // new JoystickButton(m_weapons, Button.kRightBumper.value).onTrue(new AutoScore(m_PinkArm, m_arm, m_gripper, kHighAngleCube, HighExtendCube));
-    // new JoystickButton(m_weapons, (int) m_weapons.getRightTriggerAxis()).onTrue(new AutoScore(m_PinkArm, m_arm, m_gripper, kMidAngleCube, MidExtendCube));
+    new JoystickButton(m_weapons, Button.kRightBumper.value).onTrue(new AutoScore(m_PinkArm, m_arm, m_gripper, kHighAngleCube, HighExtendCube));
+    new JoystickButton(m_weapons, (int) m_weapons.getRightTriggerAxis()).onTrue(new AutoScore(m_PinkArm, m_arm, m_gripper, kMidAngleCube, MidExtendCube));
   }
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
