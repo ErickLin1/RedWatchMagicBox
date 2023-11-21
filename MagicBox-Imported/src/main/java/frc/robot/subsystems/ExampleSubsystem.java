974ddf4d66c0d207a73bac6a0fa3ea638e769313
AnirudSriram <122627@lrstudents.org>
2023-11-21 17:22:42 -0500

    Created String Pot Challenge Branch
    Created code for the string potentiometer and the subsystem for the motor

--- MagicBox-Imported/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ MagicBox-Imported/src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -3,11 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ExampleSubsystem extends SubsystemBase {
