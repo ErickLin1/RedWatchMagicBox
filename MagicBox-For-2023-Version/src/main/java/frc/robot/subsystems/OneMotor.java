f4536509c95c563d1a097da79b297f9879e5aef9
KrithikAlluri <118485611+KrithikAlluri@users.noreply.github.com>
2024-02-08 15:36:16 -0500

    Add Shuffleboard

--- MagicBox-For-2023-Version/src/main/java/frc/robot/subsystems/OneMotor.java
+++ MagicBox-For-2023-Version/src/main/java/frc/robot/subsystems/OneMotor.java
@@ -5,15 +5,28 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.RelativeEncoder;
+
+import java.util.Map;
+
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.RunMotor;
 
 public class OneMotor extends SubsystemBase {
   /** Creates a new OneMotor. */
 
+  private final GenericEntry setMotorEncoder;
+  private final ShuffleboardTab m_controlpanelTab;
+
+  private final ShuffleboardLayout m_motorStatus;
   public final com.revrobotics.CANSparkMax m_motor;
   public final RelativeEncoder m_encoder;
 
@@ -25,7 +38,18 @@ public class OneMotor extends SubsystemBase {
 
     m_encoder = m_motor.getEncoder();
     m_encoder.setPosition(0);
-    
+    m_controlpanelTab = Shuffleboard.getTab("Control Panel");
+
+    m_motorStatus = m_controlpanelTab.getLayout("Motor Status", BuiltInLayouts.kList)
+      .withProperties(Map.of("Label position", "TOP"))
+      .withPosition(0, 0)
+      .withSize(2, 4);
+      
+    m_motorStatus.addNumber("Motor Speed", () -> getSpeed()); // Angle of shooter
+    setMotorEncoder = m_motorStatus.add("Set Motor Speed", getSpeed()).getEntry();
+    m_motorStatus.add(new RunMotor(this, setMotorEncoder.get().getDouble()));  
+
+
   }
 
   public void setSpeed(double speed) {
