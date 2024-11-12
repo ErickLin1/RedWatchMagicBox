39d38e27fc11814959a7ff1cd1ddd63121daacf4
ErickLin1 <172626@lrstudents.org>
2024-11-12 16:57:47 -0500

    Continued Developing Limit Switch
    Fixed some imports and added code.

--- MagicBox/src/main/java/frc/robot/LimitSwitchMotor.java
+++ MagicBox/src/main/java/frc/robot/LimitSwitchMotor.java
@@ -1,38 +1,59 @@
 package frc.robot;
 
-// need to figure out how to import NEOs
-// import edu.wpi.first.wpilibj.neos;
-import edu.wpi.first.wpilibj.TimedRobot;
 
+
+//package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.DriverStation;
 
 import com.revrobotics.SparkMaxLimitSwitch;
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
+
 
 DigitalInput topLimitSwitch = new DigitalInput(0);
 DigitalInput bottomLimitSwitch = new DigitalInput(1);
 Joystick stick = new Joystick(0);
 
-public class LimitSwitchMotor {
+public class LimitSwitchMotor extends TimedRobot {
     // neos motor1 = new neo(0);
     Joystick stick;
     DigitalInput limitSwitch;
+    CANSparkMax leftMotor;
+    CANSparkMax rightMotor;
+    SparkMaxLimitSwitch forward_limit_L;
+    SparkMaxLimitSwitch reverse_limit_L;
+    SparkMaxLimitSwitch forward_limit_R;
+    SparkMaxLimitSwitch reverse_limit_R;
 
+
+        
+
+
+    @Override
     public void robotInit() {
+        leftMotor = new CANSparkMax(2, MotorType.kBrushless);
+        rightMotor = new CANSparkMax(2, MotorType.kBrushless);
+
+        forward_limit_L.enableLimitSwitch(false);
+        reverse_limit_L.enableLimitSwitch(false);
+        forward_limit_R.enableLimitSwitch(false);
+        reverse_limit_R.enableLimitSwitch(false);
         
         
     }
    
-    public void autonomousInit() {
+    public void teleopPeriodic() {
     
     }
 
